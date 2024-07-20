1deca239b2fa8fabecb2dac1bb5b41b602649a57
Ahmed A Sulaiman <132774621+Ahmed-Sulaiman@users.noreply.github.com>
2024-07-20 09:32:35 -0400

    custom Hooks

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClinet from "../services/api-clinet";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+interface FetchGameResponse {
+    count: number;
+    results: Game[];
+  }
+
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClinet
+      .get<FetchGameResponse>("/games", {signal: controller.signal})
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)});
+        
+      return () => controller.abort()
+  }, []);
+
+  return {games, error};
+
+
+}
+
+export default useGames;
